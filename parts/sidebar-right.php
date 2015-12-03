a2f67d1f18d2bd1ec3f5d7cd921bf71a4c5da201
shaanamin13 <shaan-amin@uiowa.edu>
2015-12-02 23:58:50 -0600

    Several Changes on Ilmfinity

---
+++ parts/sidebar-right.php
@@ -0,0 +1,69 @@
+<div class="large-3 columns">
+	<div class="row">
+		<div class="social-media-side">
+			<h4 class="subheader">Social Media</h4>
+			<div class="twitter-follow">
+				<a class="twitter-follow-button"
+				href="https://twitter.com/ilmfinity"
+				data-size="small">
+				Follow @Ilmfinity</a>
+			</div>
+			<div class="fb-like" data-href="https://www.facebook.com/ilmfinity/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+
+		</div>
+
+	</div>
+
+	<div class="row">
+		<div class="social-media-side">
+			<h4 class="subheader">Contact Us</h4>
+			<p>Have a question for us?</p>
+			<a href="#" data-reveal-id="firstModal" class="radius small button">Send an Email!</a>
+
+			<div id="firstModal" class="reveal-modal" data-reveal>
+				<h2 id="form">Send a Message.</h2>
+				<form>
+					<fieldset>
+						<legend>Message Details</legend>
+
+						<div class="row">
+							<div class="large-6 columns">
+								<label>Your E-mail Address: </label>
+								<input type="text">
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="large-6 columns">
+								<label>Full Name</label>
+								<input type="text" >
+							</div>
+								</div>
+						<div class ="row">
+							<div class="large-6 columns">
+								<label>Subject</label>
+								<input type="text">
+							</div>
+						</div>
+
+
+
+						<div class="row">
+							<div class="large-12 columns">
+								<label>Message</label>
+								<textarea></textarea>
+							</div>
+						</div>
+						<div class="row">
+							<div class="large-12 columns">
+									<a href="#" class="small alert button round">Cancel</a>
+									<a href="#" class="small success button round">Submit</a>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+
+		</div>
+	</div>
+</div>
--- templates/front.php
+++ templates/front.php
@@ -4,18 +4,27 @@ Template Name: Front
 */
 get_header(); ?>
 
+<!-- fb share buttons -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=463583370371382";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<!-- Front page header components -->
 <header id="front-hero" role="banner">
 
 	<div class="row">
 		<div class="medium-6 columns">
-
 		</div>
-
 		<div class="medium-6 columns">
 			<div class="row">
-
 				<a href="http://www.evocreo.com" title="EvoCreo"><img src=<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ilmfinity/EvoCreoTitle.png></a></h1>
-
 			</div>
 			<div class="marketing">
 
@@ -28,49 +37,54 @@ get_header(); ?>
 						<a href="https://itunes.apple.com/us/app/evocreo-a-monster-capture-rpg/id918997182?mt=8">	<img class="appStore" src=<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ilmfinity/available-appstore-small.png></a>
 
 					</div>
-
 				</div>
-
 				<div id="watch" class="small-12 columns">
-
 				</div>
-
-
-
 			</div>
 		</div>
 	</div>
-
 </header>
 
-</div>
 <div class="row">
-	<?php get_template_part( 'parts/check-if-sidebar-exist' ); ?>
-	<?php do_action( 'foundationpress_before_content' ); ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-			</header>
-			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php do_action( 'foundationpress_page_before_comments' ); ?>
-			<?php comments_template(); ?>
-			<?php do_action( 'foundationpress_page_after_comments' ); ?>
-		</article>
-	<?php endwhile;?>
+	<div class="small-12 columns large-9 columns">
 
-	<?php do_action( 'foundationpress_after_content' ); ?>
+		<?php $my_query = "showposts=3"; $my_query = new WP_Query($my_query); ?>
+		<?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+			<article class="front-post" id="post-<?php the_ID(); ?>">
+				<?php
+				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+					the_post_thumbnail();
+				}
+				?>
+				<header>
+					<h4 class="entry-title"><?php the_title(); ?></h4>
+				</header>
+				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+
+					<footer class="post-footer">
+						<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+						<p><?php the_tags(); ?></p>
+						<ul>
+							<li>Author: Shaan Amin</li>
+							<li>Date: 11/25/2015</li>
+						</ul>
+					</footer>
+
+			</article>
+
+		<?php endwhile; // end of one post ?>
+	<?php endif; //end of loop ?>
 
 </div>
+<!-- Sidebar -->
 
-<?php get_sidebar(); ?>
+<?php get_template_part( 'parts/sidebar-right' ); ?>
+
+
 </div>
 <?php get_footer(); ?>
